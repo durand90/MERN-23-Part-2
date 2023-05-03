3299fa316fcd60523661eba46424cbb34fc7b385
Fanfan durand <durand90@live.com>
2023-05-03 19:26:17 -0400

    second

--- a.js
+++ a.js
@@ -1,1 +1,20 @@
-console.log(magicUnicorns);
+
+
+/*
+console.log(magicalUnicorns);
+var magicalUnicorns = "awesome";
+*/
+
+/*
+console.log(magicalUnicorns);
+let magicalUnicorns = "awesome!";
+*/
+
+var foo = "bar";
+function magic(){
+    foo = "hello world";
+    console.log(foo);
+    var foo;
+}
+magic();
+console.log(foo);
